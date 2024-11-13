0ce66bf182c68f8c81e29f39ee70e9d921f97626
PedroRibeiro3A <00001105510918sp@al.educacao.sp.gov.br>
2024-11-13 11:29:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Naruto',
+    'Qual manto de caudas deixou a cicatriz no peito do jiraya?',
+    '4 caudas'
+)
+
+criaCartao(
+    'Naruto',
+    'Quais são os 3 ninjas que participam da técnica da Formação do Trovão Voador ?',
+    'Raidou Namiashi, Genma Shiranui, Iwashi Tatami'
+)
+
+criaCartao(
+    'Naruto',
+    'Minato e Hashirama são ninja do tipo?',
+    'Sensorial'
+)
+
+criaCartao(
+    'Naruto',
+    'quem foi o primeiro ninja a treinar com os sapos do monte myoboko?',
+    'Hagoromo'
+)
